7d4ff163b999a18e7ad17613c79ec34cbfa160f3
SREENADTH KRISHNA <134028402+LuciferKingOfCoders@users.noreply.github.com>
2024-06-03 04:07:14 -0400

    IMS_C1

---
+++ ims_software/app/(back_End)/dashboard/Home/page.jsx
@@ -0,0 +1,11 @@
+import { Grenze } from "next/font/google";
+import React from "react";
+
+export default function page() {
+    return (
+        <dev>
+            <h2>Home</h2>
+        </dev>
+    )
+}
+
---
+++ ims_software/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h2 className="text-3xl">Inventory Management System</h2>
+    </div>
+  );
+}
